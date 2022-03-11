47ef635d514e982355eb908c8f640ac3be566f89
Ravi <raviramchand8@gmail.com>
2022-03-11 14:46:23 -0500

    dummy records

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Teams(TeamName, Continent, Played, Won, Drawn, Lost)
+VALUES ('France', 'Europe', 7, 6, 1, 0),
+       ('Belgium', 'Europe', 7, 6, 0, 0),
+       ('Croatia', 'Europe', 7, 4, 2, 1),
+       ('Uruguay', 'S.America', 5, 4, 0, 1),
+       ('England', 'Europe', 7, 3, 1, 3);
+
